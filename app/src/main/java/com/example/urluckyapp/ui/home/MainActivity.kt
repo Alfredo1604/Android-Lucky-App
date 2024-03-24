bc9d5b8a3a23d70fa846d831cd6546afea63ae2f
Alfredo1604 <arr_16@hotmail.com>
2024-03-24 13:15:29 -0600

    initial commit

---
+++ app/src/main/java/com/example/urluckyapp/ui/home/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.urluckyapp.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.urluckyapp.R
+import com.example.urluckyapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun initUI(){
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        val navHost = supportFragmentManager.findFragmentById(R.id.FragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomBarNavigationView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/urluckyapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.urluckyapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.urluckyapp.R
+import com.example.urluckyapp.databinding.FragmentHoroscopeBinding
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
